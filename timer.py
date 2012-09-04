f777a17923d704b6bc8ab884509536660c86aa83
Steven D'Aprano <steve+python@pearwood.info>
2012-09-05 02:57:06 +1000

    Minor changes to timer

--- timer.py
+++ timer.py
@@ -8,15 +8,18 @@ class Timer:
         self.disable_gc = disable_gc
         self.verbose = verbose
         self.start = self.end = self.interval = None
+
     def __enter__(self):
         if self.disable_gc:
-            self.gc_state = gc.isenabled()
+            self._gc_state = gc.isenabled()
             gc.disable()
+        self.interval = None
         self.start = self.timer()
         return self
+
     def __exit__(self, *args):
         self.end = self.timer()
-        if self.disable_gc and self.gc_state:
+        if self.disable_gc and self._gc_state:
             gc.enable()
         self.interval = self.end - self.start
         if self.verbose:
